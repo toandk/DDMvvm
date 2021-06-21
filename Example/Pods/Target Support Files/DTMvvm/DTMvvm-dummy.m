9c09aa42bc505451f1bbcd1f2826ad3616ce53bc
starfall_9000 <starfall.9000.21@gmail.com>
2021-06-21 10:58:03 +0700

    remove pods folder

--- Example/Pods/Target Support Files/DTMvvm/DTMvvm-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DTMvvm : NSObject
-@end
-@implementation PodsDummy_DTMvvm
-@end
--- Example/Pods/Target Support Files/Pods-DTMvvm_Example/Pods-DTMvvm_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DTMvvm_Example : NSObject
-@end
-@implementation PodsDummy_Pods_DTMvvm_Example
-@end
